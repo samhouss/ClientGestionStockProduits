85b50b237de97e7f5bae79bd1eb7dfb50e87beec
samhouss <sami_puyol@live.fr>
2019-08-13 12:46:06 +0100

    ajout du navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ProduitMockService } from './produit/produit.mock.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProduitComponent,
+        NavbarComponent,
+        ContentComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [ProduitMockService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
